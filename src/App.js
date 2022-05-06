6b7ea97b6942092523c3a9ea139db6bcdd3a38d1
sallemghouil <06243299ghouil>
2022-05-06 12:29:21 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from './Cards/Cards';
+import Nav from './Nav/Nav';
+function App() {
+  return (
+   <div>
+<Card/>
+<Nav/>
+   </div>
+  );
+}
+
+export default App;
